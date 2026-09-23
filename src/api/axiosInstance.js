0bab37ec57f3c165a3bdd8baa02f7ed1cf5e2a7b
Sarvagya Singhal <dhruvsinghal166@gmail.com>
2026-09-23 23:27:43 +0530

    feat: setup vite project and axios instance with auth interceptor

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+// 1. Base URL config
+const axiosInstance = axios.create({
+  baseURL: 'https://dummyjson.com',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 2. Request Interceptor (Automatically token add karega)
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
